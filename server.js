fd7f0d10d629787e18622a4c123b27bf213cdd22
Piyush Dubey <dubeypiyush@microsoft.com>
2023-02-15 22:05:02 +0530

    list hacks

--- server.js
+++ server.js
@@ -18,12 +18,26 @@ app.get('/', (req, res) => {
     })
     .catch(err=>{
         console.log(err)
+        res.render('error')
     })
     
 });
 
+app.get('/detail/:hackathonId', (req, res) => {
+    let hackathonId = req.params.hackathonId;
+    client.get('https://mocki.io/v1/b3c7624f-7e32-41c1-9f1a-c941546ae3ff')
+    .then(apiResp=>{
+        console.log(apiResp.data)
+        res.render('index',{ active: "detail", data: apiResp.data });
+    })
+    .catch(err=>{
+        console.log(err)
+        res.render('error')
+    })
+
+})
 app.get('/index', (req, res) => {
-    res.render('index', { active: "index" });
+    res.render('/', { active: "index" });
 });
 
 app.get('/create', (req, res) => {
