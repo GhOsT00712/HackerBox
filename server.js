77967eb8113771ee1e383e57cb4dd18ad4828ba1
Piyush Dubey <dubeypiyush@microsoft.com>
2023-02-15 20:41:40 +0530

    hackathon lists

--- server.js
+++ server.js
@@ -3,14 +3,23 @@ const app = express();
 const port = 8080;
 const fs = require('fs');
 const path = require('path');
+const client = require('axios');
+const { response } = require('express');
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "views")));
 app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
-    console.log("index page")
-    res.render('index',{ active: "index" });
+    client.get('https://mocki.io/v1/1df3d356-af3d-457a-a6bf-1c31c0d1dd73')
+    .then(apiResp=>{
+        console.log(apiResp.data)
+        res.render('index',{ active: "index", data: apiResp.data });
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+    
 });
 
 app.get('/index', (req, res) => {
